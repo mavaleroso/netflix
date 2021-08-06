83f6776984d8ab66e76ea124e9a3a7e1e6e28f9b
Marwen Valeroso <marwenvalerosO@gmail.com>
2021-08-06 17:22:38 +0800

    changes_1 : Change 2nd Layout

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <Nav />
-    <!-- <Home /> -->
+    <Home />
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-// import Home from "./components/Home.vue";
+import Home from "./components/Home.vue";
 import Nav from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
-    // Home,
+    Home,
     Nav,
   },
 };
